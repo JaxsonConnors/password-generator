a6badb510e7b8b4300a31abe6ae0590df45a5176
Jaxson Connors <connorsjaxson@gmail.com>
2021-08-12 22:45:00 -0600

    finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,50 +11,100 @@ const num = '0123456789';
 const spec = ' !#$%&()*+,-./:;<=>?@[\]^_`{|}~';
 
 function allowedCharacters() {
-  let charset = ''
+  let ret = { alphaU: false, alphaL: false, spec: false, num: false, charSet: ''}
 
   var passL = window.confirm('Do you want lowercase letters in your password?');
   if (passL) {
-  var charset += alphaL
+    ret.alphaL = true;
+    ret.charSet += alphaL;
   }
 
   var passU = window.confirm('Do you Want Capital letters in your password?');
   if (passU) {
-    charset += alphaU
+    ret.alphaU = true;
+    ret.charSet += alphaU;
   }
 
   var passNum = window.confirm('Do you want numbers in your password?');
   if (passNum) {
-    charset += num
+    ret.num = true;
+    ret.charSet += num;
   }
 
   var passSpec = window.confirm('Do you want special characters (+/?; etc...) in your password?');
   if (passSpec) {
-    charset += spec
+    ret.spec = true;
+    ret.charSet += spec;
   }
 
-  return charset;
+  return ret;
 };
 
 function generatePassword() {
-
   var passLength = '';
 
   do {
     passLength = parseInt(window.prompt('How long would you like your password to be?'));
   } while (passLength < 8 || passLength > 128);
 
-  let length = passLength,
-    charset = allowedCharacters();
-
-    let hasPassed = false;
+  let length = passLength;
+  let charSets = allowedCharacters();
+  let hasLower = false,
+      hasUpper = false,
+      hasNum   = false,
+      hasSpec  = false;
+  let password = '';
 
   do {
-    let password = '';
-    for (let i = 0, n = charset.length; i < length; i++) {
-      password += charset.charAt(Math.floor(Math.random() * n));
+    for (let i = 0, n = charSets.charSet.length; i < length; i++) {
+      password += charSets.charSet.charAt(Math.floor(Math.random() * n));
     }
-  } while (!hasPassed);
+
+    if (charSets.alphaL) {
+      for (let c of password) {
+        for (let c1 of alphaL) {
+          if (c === c1) {
+            hasLower = true;
+            break;
+          }
+        }
+      }
+    } else hasLower = true;
+
+    if (charSets.alphaU) {
+      for (let c of password) {
+        for (let c1 of alphaU) {
+          if (c === c1) {
+            hasUpper = true;
+            break;
+          }
+        }
+      }
+    } else hasUpper = true;
+
+    if (charSets.num) {
+      for (let c of password) {
+        for (let c1 of num) {
+          if (c === c1) {
+            hasNum = true;
+            break;
+          }
+        }
+      }
+    } else hasNum = true;
+
+    if (charSets.spec) {
+      for (let c of password) {
+        for (let c1 of spec) {
+          if (c === c1) {
+            hasSpec = true;
+            break;
+          }
+        }
+      }
+    } else hasSpec = true;
+
+  } while (!hasLower || !hasUpper || !hasNum || !hasSpec);
 
   return password;
 };
